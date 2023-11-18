5581d3539abca4bcc55c38bbde311275fc200eda
as <as.com>
2023-11-18 15:21:20 +0300

    waveglow

--- hw_tts/utils/util.py
+++ hw_tts/utils/util.py
@@ -281,9 +281,13 @@ def get_WaveGlow():
         Path(waveglow_path).mkdir(exist_ok=True, parents=True)
         download_file("https://drive.google.com/u/0/uc?id=1WsibBTsuRg_SF2Z6L6NFRTT-NjEy1oTx", os.path.join(waveglow_path, "waveglow_256channels.pt"))
     waveglow_path = os.path.join(waveglow_path, "waveglow_256channels.pt")
-    wave_glow = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_waveglow', model_math='fp32')
+    # wave_glow = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_waveglow', model_math='fp32')
+    wave_glow = torch.load(waveglow_path)['model']
     wave_glow = wave_glow.remove_weightnorm(wave_glow)
-    wave_glow.cuda().eval()
+    if torch.cuda.is_available():
+        wave_glow.cuda().eval()
+    else:
+        wave_glow.cpu().eval()
     for m in wave_glow.modules():
         if 'Conv' in str(type(m)):
             setattr(m, 'padding_mode', 'zeros')
